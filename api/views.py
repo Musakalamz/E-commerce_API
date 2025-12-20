b51076e1f8fdefd8994ceb6aca2d536de806d8b9
Musakalamz <ogunsolumusa@gmail.com>
2025-12-20 17:36:40 +0100

    feat: implement product viewset with search and ownership

--- api/views.py
+++ api/views.py
@@ -6,3 +6,14 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'category__name']
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
