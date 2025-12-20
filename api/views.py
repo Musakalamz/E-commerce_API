00526505b5b2f36ddb3d982f6f17d35f4472b10a
Musakalamz <ogunsolumusa@gmail.com>
2025-12-20 21:30:17 +0100

    feat: add filtering by category and stock status

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -13,7 +14,8 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['category', 'stock']
     search_fields = ['name', 'category__name']
 
     def perform_create(self, serializer):
