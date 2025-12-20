4ba4e52c678bc63fc22f68ce0d02d6aa562b1f45
Musakalamz <ogunsolumusa@gmail.com>
2025-12-20 17:42:27 +0100

    feat: add user registration logic to serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,21 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Category, Product
 
+
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
