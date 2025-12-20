bbe5ee628e14847d85cfd132c1a5f0311080fb59
Musakalamz <ogunsolumusa@gmail.com>
2025-12-20 17:58:30 +0100

    test: add integration tests for product endpoints

---
+++ api/tests/test_products.py
@@ -0,0 +1,27 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from .factories import create_user, create_category, create_product
+
+class ProductAPITests(APITestCase):
+    def setUp(self):
+        self.user = create_user()
+        self.category = create_category()
+        self.product = create_product(self.category, self.user)
+        self.url = reverse('product-list')
+
+    def test_search_products(self):
+        response = self.client.get(self.url, {'search': 'Laptop'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        
+    def test_create_product_unauthorized(self):
+        data = {
+            'name': 'Tablet',
+            'description': 'New tablet',
+            'price': 200.00,
+            'stock': 5,
+            'category': self.category.id
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
