56b58dc59ef06378348f29e9e0d5ede39133ea01
Musakalamz <ogunsolumusa@gmail.com>
2025-12-20 17:24:41 +0100

    feat: implement category model definition

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
