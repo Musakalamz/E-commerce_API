f4d6ef33aecfe5d658429bf43e93bfaa3b41139e
Musakalamz <ogunsolumusa@gmail.com>
2025-12-20 22:01:49 +0100

    feat: add jwt token endpoints

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView # Add this import
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', RedirectView.as_view(url='api/', permanent=False)), # Add this line
+    path('', RedirectView.as_view(url='api/', permanent=False)), 
+     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
